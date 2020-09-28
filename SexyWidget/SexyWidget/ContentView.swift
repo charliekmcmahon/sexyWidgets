2c471e2c2aeb951c839d40b75ed9c861a18bde89
Charlie McMahon <charliekmcmahon@gmail.com>
2020-09-28 11:18:21 +1000

    Add initial Stub files

---
+++ SexyWidget/SexyWidget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SexyWidget
+//
+//  Created by Charlie McMahon on 28/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
